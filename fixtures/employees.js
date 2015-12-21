7652157c9a0321bf4acb98babbf30d58799d604f
migue <miguelisaac1985@hotmail.com>
2015-12-21 11:40:05 -0300

    Se agrego fixture, estrategia local, login con autenticacion y securización de las rutas

---
+++ fixtures/employees.js
@@ -0,0 +1,5 @@
+exports.Employees = [
+    { name: 'Gary', lastName: 'Kasparov', email: 'garyKasparov@gmail.com' },
+    { name: 'Antatoli', lastName: 'Karpov', email: 'anatoliKarpov@gmail.com' },
+    { name: 'Miguel', lastName: 'Najdorf', email: 'migueNajdorf@gmail.com' }
+];
--- routes/main.js
+++ routes/main.js
@@ -1,17 +1,38 @@
 var app = module.parent.exports.app;
+var passport = module.parent.exports.passport;
 var Employees = require('../models/employees.js');
+var Admins = require('../models/admins.js');
 
-app.get('/panel', function(req, res){
+var adminAuth = function (req, res, next) {
+  if(typeof req.user != "undefined"){
+    next();
+  }else{
+    res.redirect('/');
+  }
+}
+
+app.get('/login', function(req, res){
+  res.render('login', { title: 'Login'});
+});
+
+app.post('/login', passport.authenticate('AdminLogin', 
+  { successRedirect: '/panel',
+    failureRedirect: '/login',
+    failureFlash: true }));
+
+app.get('/panel', adminAuth, function(req, res){
+  var msg = req.flash('message');
   Employees.find({}, function(err, docs){
-    res.render('panel', { title: 'Panel', employees: docs});
+    res.render('panel', { title: 'Panel', employees: docs, flashmsg: msg});
   });
 });
 
-app.get('/panel/employees/new', function(req, res){
+app.get('/panel/employees/new', adminAuth, function(req, res){
+  req.flash('message', 'Sección: Nuevo empleado.');
   res.render('new', { title: 'New'});
 });
 
-app.post('/panel/employees/new', function(req, res){
+app.post('/panel/employees/new', adminAuth, function(req, res){
   console.log(req.body);
   var e = new Employees({ name: req.body.name,
                         lastName: req.body.lastName,
@@ -27,7 +48,7 @@ app.post('/panel/employees/new', function(req, res){
     });
 });
 
-app.get('/panel/employees/delete/:id', function (req, res) {
+app.get('/panel/employees/delete/:id', adminAuth, function (req, res) {
   Employees.remove({_id: req.params.id}, function (err, doc) {
     if(!err){
       res.redirect('/panel');
@@ -37,7 +58,7 @@ app.get('/panel/employees/delete/:id', function (req, res) {
   });
 });
 
-app.get('/panel/employees/edit/:id', function (req, res){ 
+app.get('/panel/employees/edit/:id', adminAuth, function (req, res){ 
   Employees.findOne({ _id: req.params.id }, function(err, doc){
         if(!err){
             res.render('edit', { title: 'Edit', employee: doc});
@@ -47,7 +68,7 @@ app.get('/panel/employees/edit/:id', function (req, res){
     });
 });
 
-app.post('/panel/employees/edit/:id', function (req, res) {
+app.post('/panel/employees/edit/:id', adminAuth, function (req, res) {
   Employees.findOne({ _id: req.params.id }, function (err, doc) {
     if(!err){
       doc.name = req.body.name;
